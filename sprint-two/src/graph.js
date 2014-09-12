773406a34fe06bfce0fedd47308e2efe017675d2
jjmerino <jomerinog@gmail.com>
2014-09-12 12:21:07 -0700

    finished graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,48 +1,92 @@
 var Graph = function(){
-  this.nodes = [];
+  this.nodes = {};
+
+  this.makeNode = function(value){
+    return {
+      value: value,
+      edges: {}
+    };
+  };
+
 
 };
-var makeNode = function(value){
-  return {
-    value: value,
-    edges: []
-  };
-};
+
 
 Graph.prototype.addNode = function(newNode, toNode){
-  var realNewNode =makeNode(newNode);
-  this.nodes.push(realNewNode);
+  var realNewNode =this.makeNode(newNode);
+  var keys=Object.keys(this.nodes);
+  var isSecondNode = keys.length===1;
+  this.nodes[newNode]=realNewNode;
+
+  //if this is the second node
+  if(isSecondNode){
+    // add edge from first node to the newNode.
+    this.addEdge(keys[0],newNode);
+  }
+
+
   if(toNode!== undefined){
-    var realToNode = _.find(this.nodes,function(item){
-      return item.value === toNode;
-    });
+    var realToNode = this.nodes[toNode];
     if(realToNode !== undefined){
-      realNewNode.edges.push(realToNode);
-      realToNode.edges.push(realNewNode);
+      realNewNode.edges[toNode] = realToNode;
+      realToNode.edges[newNode] = realNewNode;
     }
   }
 };
 
 Graph.prototype.contains = function(node){
-  return undefined !== _.find(this.nodes,function(item){
 
-    return item.value === node;
-  });
+  return undefined !== this.nodes[node];
+
 };
 
 Graph.prototype.removeNode = function(node){
-  this.node = _.reject(this.nodes,function(val){
-    return val.value === node;
+  _.each(this.nodes,function(item){
+    delete item.edges[node];
   });
+  delete this.nodes[node];
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  var realFromNode = this.nodes[fromNode];
+  var realToNode = this.nodes[toNode];
+  if(realFromNode===undefined||realToNode===undefined){
+    return false;
+  }
+  return realFromNode.edges[toNode]!==undefined&&realToNode.edges[fromNode]!==undefined;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  var realFromNode = this.nodes[fromNode];
+  var realToNode = this.nodes[toNode];
+  if(realFromNode===undefined || realToNode===undefined){
+    return;
+  }
+  realFromNode.edges[toNode]=true;
+  realToNode.edges[fromNode]=true;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var realFromNode = this.nodes[fromNode];
+  var realToNode = this.nodes[toNode];
+  if(realFromNode===undefined || realToNode===undefined){
+    return;
+  }
+  delete realFromNode.edges[toNode];
+  delete realToNode.edges[fromNode];
+  var that = this;
+  _.each([fromNode,toNode],function(theNode){
+    var theNodeHasEdges = _.some(that.nodes,function(item){
+      return item.edges[theNode] !== undefined;
+    });
+
+    if(!theNodeHasEdges){
+      delete that.nodes[theNode];
+    }
+
+  });
+
+
 };
 
 /*
