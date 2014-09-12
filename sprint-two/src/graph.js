902231792e41e61176664ebfbacb56a4e7f4c9d2
jjmerino <jomerinog@gmail.com>
2014-09-11 22:21:49 -0700

    Completed addNode function

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,13 +1,39 @@
 var Graph = function(){
+  this.nodes = [];
+
+};
+var makeNode = function(value){
+  return {
+    value: value,
+    edges: []
+  };
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  var realNewNode =makeNode(newNode);
+  this.nodes.push(realNewNode);
+  if(toNode!== undefined){
+    var realToNode = _.find(this.nodes,function(item){
+      return item.value === toNode;
+    });
+    if(realToNode !== undefined){
+      realNewNode.edges.push(realToNode);
+      realToNode.edges.push(realNewNode);
+    }
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return undefined !== _.find(this.nodes,function(item){
+
+    return item.value === node;
+  });
 };
 
 Graph.prototype.removeNode = function(node){
+  this.node = _.reject(this.nodes,function(val){
+    return val.value === node;
+  });
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
