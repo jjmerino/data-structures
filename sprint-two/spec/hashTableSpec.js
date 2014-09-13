4f9286ab4bb9f2a30c1f455a0d4865dba96c3e8b
jjmerino <jomerinog@gmail.com>
2014-09-13 16:40:08 -0700

    reformat hashtable

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -7,10 +7,11 @@ describe('hashTable', function() {
     hashTable = new HashTable();
   });
 
-  it('should have methods named "insert", "remove", and "retrieve', function() {
+  it('should have methods named "insert", "remove", "retrieve", and "size"', function() {
     expect(hashTable.insert).to.be.a("function");
     expect(hashTable.remove).to.be.a("function");
     expect(hashTable.retrieve).to.be.a("function");
+    expect(hashTable.size).to.be.a("function");
   });
 
   it('should store values that were inserted', function() {
@@ -41,26 +42,71 @@ describe('hashTable', function() {
     window.getIndexBelowMaxForKey = oldHashFunction;
   });
 
-  // (Extra credit! Remove the extra "x" when you want the following tests to run)
-  xit('should double in size when needed', function() {
-    _.each(people, function(person) {
-      var firstName = person[0], lastName = person[1];
-      hashTable.insert(firstName,lastName);
-    });
-    expect(hashTable._limit).to.equal(16);
+  it('should keep track of the size of the hashtable',function(){
+    hashTable.insert('Ryo','Osawa');
+    expect(hashTable.size()).to.equal(1);
+    hashTable.insert('Jose','Merino');
+    expect(hashTable.size()).to.equal(2);
+    hashTable.insert('Andrew','?????');
+    expect(hashTable.size()).to.equal(3);
+    hashTable.remove('Andrew');
+    expect(hashTable.size()).to.equal(2);
+
   });
 
-  xit('should halve in size when needed', function() {
+  it('should return null when retrieving unknown keys',function(){
+    expect(hashTable.retrieve('Andrew')).to.equal(null);
+  });
+
+  // (Extra credit! Remove the extra "x" when you want the following tests to run)
+  it('should double in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0], lastName = person[1];
       hashTable.insert(firstName,lastName);
     });
     expect(hashTable._limit).to.equal(16);
+    var isAllCopied = _.every(people, function(person) {
+        var firstName = person[0], lastName = person[1];
+        return hashTable.retrieve(firstName)===lastName;
+    });
+    expect(isAllCopied).to.equal(true);
+  });
+
+  it('should handle removing from empty hashtable',function(){
+    hashTable.remove('something');
+    expect(hashTable.size()).to.equal(0);
+    hashTable.insert('foo','bar');
+    expect(hashTable.size()).to.equal(1);
+    hashTable.remove('someone'); // removed something that we didnt add
+    expect(hashTable.size()).to.equal(1);
+    hashTable.remove('foo');
+    expect(hashTable.size()).to.equal(0);
+    hashTable.remove('foo'); // removing foo that we already removed
+    expect(hashTable.size()).to.equal(0);
+  });
+
+  it('should halve in size when needed', function() {
+    _.each(people, function(person) {
+      var firstName = person[0], lastName = person[1];
+      hashTable.insert(firstName,lastName);
+    });
+    expect(hashTable._limit).to.equal(16);
+    var namesToRemove = ['George', 'Dr.', 'Steven', 'John', 'Mr.'];
     hashTable.remove('George');
     hashTable.remove('Dr.');
     hashTable.remove('Steven');
     hashTable.remove('John');
     hashTable.remove('Mr.');
     expect(hashTable._limit).to.equal(8);
+
+    var isAllCopied = _.chain(people)
+    .filter(function(person) {
+      return _.contains(namesToRemove, person);
+    })
+    .every(function(person) {
+        var firstName = person[0], lastName = person[1];
+        return hashTable.retrieve(firstName)===lastName;
+    }).value();
+    expect(isAllCopied).to.equal(true);
   });
 });
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,35 +1,89 @@
 var HashTable = function(){
-  this._limit = 8;
+  this._minLimit = 8;
+  this._limit = this._minLimit;
   this._storage = makeLimitedArray(this._limit);
+  this._size = 0;
+};
+
+HashTable.prototype._rehash = function(newLimit){
+  var oldStorage = this._storage;
+  this._limit = newLimit;
+  this._size = 0;
+  this._storage = makeLimitedArray(this._limit);
+
+  //rehash all the existing elements
+  oldStorage.each(function(bucket){
+    if(!bucket) { return; }
+    for(var j = 0; j < bucket.length; j++){
+      //insert the new value.
+      this.insert(bucket[j][0], bucket[j][1]);
+    }
+  }.bind(this));
 };
 
 HashTable.prototype.insert = function(k, v){
+
+  // get a hashed index for the value
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  // get the bucket for the given index
   var bucket = this._storage.get(i);
-  if (bucket === undefined || bucket === null) {
 
-    // TODO: may want to implement it as an array.1
-    bucket = {};
+  // check that the bucket exists
+  if (!bucket) {
+    bucket = [];
     this._storage.set(i, bucket);
   }
 
-  bucket[k] = v;
+  bucket.push([k,v]);
+
+  this._size++;
+
+  // if size is more than 75% rehash
+  if(this._size > this._limit * 0.75){
+    // if so double the size (limit)
+    this._rehash(this._limit * 2);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
-  return bucket[k];
+  if(!bucket) { return null; }
+
+  for(var j = 0 ; j < bucket.length ; j++){
+    if(bucket[j][0]===k){
+      return bucket[j][1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
-  if (bucket === undefined || bucket === null) {
-    return;
+  if(!bucket) { return; }
+
+  for(var j = 0 ; j < bucket.length ; j++){
+    if(bucket[j][0]===k){
+      this._size--;
+      bucket.splice(j,1);
+
+      // if size-1 is less than 25%
+      if(this._size < this._limit * 0.25){
+        // if so half the size (limit)
+        var newLimit = Math.floor(this._limit / 2);
+        newLimit = newLimit < this._minLimit ? this._minLimit : newLimit;
+        this._rehash(newLimit);
+      }
+      return;
+    }
   }
-  bucket[k] = null;
+};
+
+HashTable.prototype.size = function(){
+  return this._size;
 };
 
 /*
