52dbf6d1d2cb8bb44025fe38de4e572e395f032a
jjmerino <jomerinog@gmail.com>
2014-09-12 20:57:40 -0700

    created new tests for linked list

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -14,6 +14,8 @@ describe('linkedList', function() {
     expect(linkedList.addToTail).to.be.a("function");
     expect(linkedList.removeHead).to.be.a("function");
     expect(linkedList.contains).to.be.a("function");
+    expect(linkedList.addToHead).to.be.a("function");
+    expect(linkedList.removeTail).to.be.a("function");
   });
 
   it('should designate a new tail when new nodes are added', function(){
@@ -52,4 +54,39 @@ describe('linkedList', function() {
   });
 
   // add more tests here to test the functionality of linkedList
+  it('should point head to the newly added head',function(){
+    linkedList.addToHead(10);
+    linkedList.addToHead(2);
+    expect(linkedList.head.value).to.equal(2);
+  });
+
+  it('should correctly set the previous value of the newtail',function(){
+    linkedList.addToTail(1);
+    expect(linkedList.tail.previous).to.equal(null);
+    linkedList.addToTail(2);
+    expect(linkedList.tail.previous.value).to.equal(1);
+    linkedList.addToTail(3);
+    expect(linkedList.tail.previous.value).to.equal(2);
+  });
+
+  it('should correctly set the next value of the newly added head',function(){
+    linkedList.addToHead(1);
+    expect(linkedList.head.next).to.equal(null);
+    linkedList.addToHead(2);
+    expect(linkedList.head.next.value).to.equal(1);
+    linkedList.addToHead(3);
+    expect(linkedList.head.next.value).to.equal(2);
+  });
+
+  it('should correctly remove tail',function(){
+    linkedList.addToHead(10);
+    linkedList.addToHead(2);
+    linkedList.removeTail();
+    expect(linkedList.tail.value).to.equal(2);
+  });
+  it('should return the value of the removed tail',function(){
+    linkedList.addToHead(10);
+    linkedList.addToHead(2);
+    expect(linkedList.removeTail()).to.equal(10);
+  });
 });
